b8ece362bcadf2e7cffe852f2d9a86471a022164
SachinAngira <56125417+SachinAngira@users.noreply.github.com>
2020-05-07 16:59:14 +0530

    Add files via upload

---
+++ Assets/Scripts/LogIn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogIn : UiTransation
+{
+    public string Usernane;
+    public string PassWard;
+    public Text User;
+    public Text pass;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnSubmit()
+    {
+        Usernane = User.text.ToString();
+        PassWard = pass.text.ToString();
+        Debug.Log(Usernane);
+        Debug.Log(PassWard);
+    }
+}
---
+++ Assets/Scripts/UiTransation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiTransation : MonoBehaviour
+{
+    public GameObject TargetUI,CurrntUI;
+
+
+    public void Translate()
+    {
+        TargetUI.SetActive(true);
+        CurrntUI.SetActive(false);
+    }
+}
